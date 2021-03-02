ee810656c37c13d5c2f07dce1e67607f74f9a1cc
JoeKing21 <67300190+JoeKing2@users.noreply.github.com>
2021-03-02 18:51:12 +0000

    added double values "h,i,j, and k" and comitted double "p,q,r, and s"

--- main.cpp
+++ main.cpp
@@ -5,9 +5,60 @@
 //------------------------------
 
 // Put the assignment code here
-
 class Quaternion {
+
+  public: double h, i, j, k;
+
+    Quaternion (double p, double q, double r, double s)
+    {
+      h=p;
+      i=q;
+      j=r;
+      k=s;
+    }
+
+    bool operator == (const Quaternion p){
+
+      if ((p->h==this->h) && p->i==this->i) && (p->j==this->j) && p->k==this->k))
+
+      return true;
+      return false;
+    }
+
+    Quaternion operator+(const Quaternion p)
+
+    {
+      Quaternion r;
+
+      r.h=p.h+this->h;
+      r.i=p.i+this->i;
+      r.j=p.j+this->j;
+      r.k=p.k+this->k;
+      return r;
+    }
+
+    Quaternion operator - (const Quaternion p)
+    {
+      Quaternion r;
+      
+      r.h=p.h-this->h;
+      r.i=p.i-this->i;
+      r.j=p.j-this->j;
+      r.k=p.k-this->k;
+      return r;
+    }
+
+    Quaternion operator * (double s)
+    {
+      Quaternion p;
+      p.h=s* this->h;
+      p.i=s* this->i;
+      p.j=s* this->j;
+      p.k=s* this->k;
+      return p;
+    }
 };
+
 
 //------------------------------
 //   DO NOT MODIFY TEST CASES
